3e6d2931ca9dbca8196bdb354f1703715bd0bd98
njaiswal <Nishant Jaiswal>
2020-01-21 14:15:45 +0000

    basic layout

---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    // this.loadScript('../../assets/js/sidebar.js');
+
+  }
+
+  // public loadScript(url: string) {
+  //   const body = <HTMLDivElement> document.body;
+  //   const script = document.createElement('script');
+  //   script.innerHTML = '';
+  //   script.src = url;
+  //   script.async = false;
+  //   script.defer = true;
+  //   body.appendChild(script);
+  // }
+}
